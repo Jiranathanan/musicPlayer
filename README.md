fca0d41c67795a1b7656e4065fa83eaee5814787
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-02-23 01:47:46 +0700

    Fix music-metadata problem, downgrade to v.7

--- README.md
+++ README.md
@@ -16,3 +16,12 @@
 #### create main.js and index.html
 #### copy and paste folder css and fonts from photon
 #### add link to ./css/photon.css
+
+
+```
+"dependencies": {
+    "electron": "^5.0.4",
+    "jquery": "^3.4.1",
+    "music-metadata": "^4.2.0"
+  }
+```
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const $ = require('jquery');
-var fs = require('fs');
-var mm = require('musicmetadata');
+const mm = require('music-metadata');
 
 function chooseMusic() {
     // console.log("Choose Music");
@@ -9,21 +8,21 @@ function chooseMusic() {
 
 function musicSelected() {
     let files = $('input').get(0).files;
+    // console.log(files[0].path)
     // console.log(typeof(files))
-    // for(let i = 0; i < files.length; i++) {
-        // let { path } = files[i];
-        // parseFile(path, {native: true})
-        //     .then((metadata) => {
-        //         console.log(metadata.common.title, metadata.common.artist, metadata.format.duration)
-        //     })
-        //     .catch(error => {
-        //         console.error('Error parsing metadata:', error);
-        //     }) 
+    for(let i = 0; i < files.length; i++) {
+        let { path } = files[i];
+        mm.parseFile(path, {native: true})
+            .then((metadata) => {
+                // console.log(metadata)
+                console.log(metadata.common.title, metadata.common.artist, metadata.format.duration)
+            })
+            .catch(error => {
+                console.error('Error parsing metadata:', error);
+            }) 
         // console.log(files[i].name, files[i].path)
-    // }
-    let parser = mm(fs.createReadStream(files[0].path), function (err, metadata) {
-        if (err) throw err;
-        console.log(metadata);
-      });
+    }
+    
+    
 }
 
